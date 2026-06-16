49e414336d1ab63357e314eca6cde45265e97b7e
Emam546 <emam54637@gmail.com>
2026-06-17 00:48:48 +0300

    fix logger

--- app/main/helpers/logger.ts
+++ app/main/helpers/logger.ts
@@ -4,10 +4,20 @@ import { jetLogger } from 'jet-logger'
 import path from 'path'
 import { isDev, isProd } from '../utils'
 import fs from 'fs'
-const logPath = path.join(app.getPath('userData'), './app.log')
+const logDir = app.getPath('userData')
+const logPath = path.join(logDir, 'app.log')
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir, { recursive: true })
+}
+console.log(logPath)
 //delete the content before the start
 fs.writeFileSync(logPath, '')
-export const logger = jetLogger({ filepath: logPath, mode: app.isPackaged ? 'file' : 'console' })
+export const logger = jetLogger({
+  filepath: logPath,
+  filepathDatetimeParam: false,
+  timestamp: false,
+  mode: app.isPackaged ? 'file' : 'console'
+})
 logger.info(process.env.NODE_ENV)
 logger.info(`production ${isProd}`)
 logger.info(`development ${isDev}`)
